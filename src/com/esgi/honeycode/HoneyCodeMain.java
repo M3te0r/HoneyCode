5270642b2d1dfc9265e8dc52d89a456c17328bf5
Que20 <maarekkevin@gmail.com>
2014-02-12 16:04:02 +0100

    Commit Que20

--- src/com/esgi/honeycode/HoneyCodeMain.java
+++ src/com/esgi/honeycode/HoneyCodeMain.java
@@ -1,18 +1,18 @@
 package com.esgi.honeycode;
 
+import javax.swing.*;
+
 /**
  * Created by Mathieu on 11/02/14.
  */
 public class HoneyCodeMain {
 
-
     public static void main(String[] args){
 
-        //test
-
-        new MainWindowUI();
-
-
-
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindowUI();
+            }
+        });
     }
 }
