595293088d7f401b8f8506e1ba3d25a4c4e26c5d
Dakstraede <alexandre.fayette@gmail.com>
2014-04-29 04:47:17 +0200

    Javadoc MainWindowUI

--- src/com/esgi/honeycode/MainWindowUI.java
+++ src/com/esgi/honeycode/MainWindowUI.java
@@ -105,6 +105,9 @@ public class MainWindowUI extends JFrame{
     private CustomConsoleOutputStream out;
 
     public MainWindowUI(){
+        /**
+         * Initialise l'interface graphique et tous ses composants
+         */
 
         setIconImage(MAIN_IMAGE);
         globalPreferences = new HCPreferences();
@@ -365,6 +368,9 @@ public class MainWindowUI extends JFrame{
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
+                /**
+                 * Fonction de fermeture de la fenêtre, via le bouton de base
+                 */
                 int confirm = JOptionPane.showConfirmDialog(JOptionPane.getFrameForComponent(exitApp), exitMessage, "Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
                 if (confirm == JOptionPane.YES_OPTION) {
                     if (tabFile.getTabCount()>0)
@@ -387,6 +393,10 @@ public class MainWindowUI extends JFrame{
 
     private void setUILanguage()
     {
+        /**
+         * Définit le langage de l'interface et initialise les composants textuels dans la langue choisie/par défaut,
+         * grâce aux fichiers de propriétés du Resource Bundle
+         */
         //Maybe moving it to class field if using other reg key ?
 
 
@@ -476,6 +486,12 @@ public class MainWindowUI extends JFrame{
 
     protected static void setNewTabTextRenamedFile(String oldTitle, String newTitle, String tooltip)
     {
+        /**
+         * Renomme un TabbedText avec le titre choisi
+         * @param oldTitle Ancien titre du TabbedText
+         * @param newTitle Nouveau titre
+         * @param tooltip chemin absolu du fichier édité dans le TabbedText
+         */
         for (int i = 0; i<tabFile.getTabCount();i++ )
         {
             if (oldTitle.equals(((JLabel)((JPanel)tabFile.getTabComponentAt(tabFile.getSelectedIndex())).getComponent(0)).getText()))
@@ -489,12 +505,21 @@ public class MainWindowUI extends JFrame{
 
     protected static void setNewTabText(String title, String tooltip)
     {
+        /**
+         * Cree un nouveau TabbedText avec le titre choisi
+         * @param title Titre du nouveau fichier choisi
+         *@param tootip Chemin absolu du fichier
+         */
+
         ((JLabel)((JPanel)tabFile.getTabComponentAt(tabFile.getSelectedIndex())).getComponent(0)).setText(title);
-        tabFile.setToolTipTextAt(tabFile.getSelectedIndex(),tooltip);
+        tabFile.setToolTipTextAt(tabFile.getSelectedIndex(), tooltip);
     }
 
     private void changeFontCurrentTab()
     {
+        /**
+         * Change la police actuelle de l'éditeur
+         */
         for(int i = 0;i<tabFile.getTabCount();i++)
         {
             ((RTextScrollPane)tabFile.getComponentAt(i)).getTextArea().setFont(new Font(globalPreferences.getFont(),Font.PLAIN,13));
@@ -503,6 +528,9 @@ public class MainWindowUI extends JFrame{
 
     private void changeThemeCurrentTab()
     {
+        /**
+         * Change le theme actuel de l'editeur
+         */
         try {
             //Themes will be modifiable in settings
             Theme theme = Theme.load(MainWindowUI.class.getResourceAsStream("/themes/"+globalPreferences.getTheme()+".xml"));
@@ -519,6 +547,16 @@ public class MainWindowUI extends JFrame{
 
     protected static void addCloseableTab(final RTextScrollPane c, final String title, final String tooltip)
     {
+        /**
+         * Ajoute un onglet fermable d'edition a la barre d'onglets
+         * @param c
+         * RTextScrollPane courant, sert au lien
+         * @param title
+         * Titre de l'onglet
+         * @param tooltip
+         * Chemin absolu du fichier correspondant
+         *
+         */
         try {
             //Themes will be modifiable in settings
             Theme theme = Theme.load(MainWindowUI.class.getResourceAsStream("/themes/"+globalPreferences.getTheme()+".xml"));
@@ -531,7 +569,7 @@ public class MainWindowUI extends JFrame{
 
 
 
-        c.getTextArea().setFont(new Font(globalPreferences.getFont(),Font.PLAIN, 13));
+        c.getTextArea().setFont(new Font(globalPreferences.getFont(), Font.PLAIN, 13));
 
         CustomCompletionProvider p = new CustomCompletionProvider("java"); //Will be replaced
         CompletionProvider provider = p.getProvider(); //An unique CompletionProvider can be used for all RSyntaxArea but not AutoCompletion
@@ -674,6 +712,11 @@ public class MainWindowUI extends JFrame{
     }
 
     private void loadPlugins(){
+        /**
+         * Charge tous les plugins .jar du dossier "plugins" et invoque les 3 premières méthodes de chacun,
+         * en leur transmettant respectivement la JMenuBar, le JTabbedPane et le JPanel de la console
+         * (modèle de développement souhaité)
+         */
         File pluginDirectory = new File(System.getProperty("user.home")+PropertiesShared.SEPARATOR+"HoneyPlugins");
         boolean created = pluginDirectory.mkdir();
         if (!created &&  !pluginDirectory.exists())
@@ -737,6 +780,11 @@ public class MainWindowUI extends JFrame{
 
     private class ActionListenerMenuBar implements ActionListener {
         public void actionPerformed (ActionEvent e){
+            /**
+             * Fonction principale de déclenchement d'évenement
+             * @param e
+             * Servira, utilisé comme e.getSource(), à localiser l'endroit du clic, et à effectuer les actions voulues
+             */
 
             if (e.getSource() == newProject)
             {
